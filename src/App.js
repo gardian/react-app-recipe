1f3791c463e04451457bffebc24bcf3d87906834
Gardian <gardianimehmeti@gmail.com>
2020-03-04 02:38:25 +0100

    using Effect and State

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = "e82fe8ea";
+  const APP_KEY = "cc6e54ca8aab31ab6ad0bc24a041feb7";
+
+  // eslint-disable-next-line no-unused-vars
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+
+    console.log('Effects has been run')
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="App">
+  <form className="search-form">
+    <input className="search-bar" type="text"/>
+    <button  className="search-button" type="submit">Search</button>
+  </form>
+  <h1 onClick={() => setCounter(counter+1)}>{counter}</h1>
+   </div>
   );
+
 }
 
 export default App;
